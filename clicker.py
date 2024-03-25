8b66739ef959d414d0005cf52981900ab3f1e0e5
1sythe <sythedev@proton.me>
2024-03-25 22:49:42 +0100

    Debug version

--- clicker.py
+++ clicker.py
@@ -1,7 +1,37 @@
 import time
 import threading
+from pynput.mouse import Button, Controller
 
-#from pynput
+
+class Clicker(threading.Thread):
+    def __init__(self, interval, mouse):
+        print("[DEBUG] AC Initialized")
+        threading.Thread.__init__(self)
+        self.interval = interval
+        self.mouse = mouse
+        self.button = Button.left
+        self.running = False
+
+    def run(self):
+        print("[DEBUG] AC Started")
+        self.running = True
+        while self.running:
+            self.mouse.click(self.button)
+            time.sleep(self.interval)
+
+        print("[DEBUG] AC Stopped")
+
+    def stop(self):
+        self.running = False
 
 
-delay = 0.25
+if __name__ == "__main__":
+    print("You now have 3 seconds to move your mouse to the desired location")
+    time.sleep(3)
+    mouse = Controller()
+    clicker = Clicker(0.25, mouse)
+    clicker.start()
+    time.sleep(5)
+    clicker.stop()
+    clicker.join()
+    print("[DEBUG] AC Finished")
