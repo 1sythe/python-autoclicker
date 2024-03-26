1a9cfaccb7f6944c575b05081c6f2111010b1494
Admin <rasmusrasello@gmail.com>
2024-03-26 02:42:52 +0100

    mouse frame, entrys (not done)

--- ui.py
+++ ui.py
@@ -30,12 +30,35 @@ def startup():
     customtkinter.CTkLabel(master=main_frame, text="Autoclicker", font=font_large).pack()
 
 
+    def on_focus_in(entry):
+        if entry.cget('state') == 'disabled':
+            entry.configure(state='normal')
+            entry.delete(0, 'end')
+
+    def on_focus_out(entry, placeholder):
+        if entry.get() == "":
+            entry.insert(0, placeholder)
+            entry.configure(state='disabled')
+
+
     mouse_frame = customtkinter.CTkFrame(master=main_frame)
     mouse_frame.place(relx=0, rely=0.12, relwidth=0.5, relheight=0.4)
 
     mouse_frame.columnconfigure((0,1,2), weight=1, uniform='a')
     mouse_frame.rowconfigure((0,1,2,3,4), weight=1, uniform='a')
 
+    customtkinter.CTkLabel(master=mouse_frame, text="Mouse", font=font_medium).grid(column=1, row=0, sticky="nswe")
+    customtkinter.CTkLabel(master=mouse_frame, text="Speed", font=font_small).grid(column=0, row=1, sticky="w", columnspan=2)
+
+    mouse_intervall_entry = customtkinter.CTkEntry(master=mouse_frame, font=font_small)
+    mouse_intervall_entry.grid(column=0, row=2)
+    mouse_intervall_entry.insert(0, "intervall in ms")
+    mouse_intervall_entry.configure(state='disabled')
+
+    mouse_cps_entry = customtkinter.CTkEntry(master=mouse_frame, font=font_small)
+    mouse_cps_entry.grid(column=2, row=2)
+    mouse_cps_entry.insert(0, "speed in cps")
+    mouse_cps_entry.configure(state='disabled')
 
     key_frame = customtkinter.CTkFrame(master=main_frame)
     key_frame.place(relx=0.5, rely=0.12, relwidth=0.5, relheight=0.4)
@@ -46,7 +69,6 @@ def startup():
 
     operating_frame = customtkinter.CTkFrame(master=main_frame)
     operating_frame.place(relx=0, rely=0.53, relwidth=1, relheight=0.25)
-
 
 
 
