9da28cbc5bce3b4a2a5c9b9ec5584bb4355e55fb
1sythe <sythedev@proton.me>
2024-03-26 03:41:49 +0100

    ui is now object orientated

--- ui.py
+++ ui.py
@@ -1,86 +1,88 @@
 import tkinter as tk
 import tkinter.font as font
-from tkinter import ttk
 import customtkinter
+import threading
+
+from tkinter import ttk
+from clicker import Clicker
+from pynput.mouse import Controller, Button
 
 
-def startup():
+class AutoClickerApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
-    root = customtkinter.CTk()
+        self.geometry('400x300')
+        self.resizable(False, False)
+        self.title("AutoClicker")
+        self.option_add("*tearOff", False)
 
-    root.geometry('400x300')
-    root.resizable(False, False)
-    root.title("AutoClicker")
-    root.option_add("*tearOff", False)
+        self.setup_visuals()
+        self.setup_ui()
 
+    def setup_visuals(self):
+        customtkinter.set_appearance_mode("dark")
+        customtkinter.set_default_color_theme("dark-blue")
 
-#visuals
-    customtkinter.set_appearance_mode("dark")
-    customtkinter.set_default_color_theme("dark-blue")
+        style = ttk.Style(self)
+        self.tk.call("source", "assets/theme.tcl")
+        style.theme_use("equilux")
 
-    style = ttk.Style(root)
-    root.tk.call("source", "assets/theme.tcl")
-    style.theme_use("equilux")
+        self.font_large = customtkinter.CTkFont(family='Comfortaa', size=25, weight='bold')
+        self.font_medium = customtkinter.CTkFont(family='Comfortaa', size=20, weight='bold')
+        self.font_small = customtkinter.CTkFont(family='Comfortaa', size=15)
+        self.font_mini = customtkinter.CTkFont(family='Comfortaa', size=10)
 
-#fonts
-    font_large = customtkinter.CTkFont(family='Comfortaa', size=25, weight='bold')
-    font_medium = customtkinter.CTkFont(family='Comfortaa', size=20, weight='bold')
-    font_small = customtkinter.CTkFont(family='Comfortaa', size=15)
-    font_mini = customtkinter.CTkFont(family='Comfortaa', size=10)
+    def setup_ui(self):
+        main_frame = customtkinter.CTkFrame(master=self)
+        main_frame.pack(pady=3, padx=3, fill="both", expand=True)
 
+        customtkinter.CTkLabel(master=main_frame, text="AutoClicker", font=self.font_large).pack()
 
-#main frame
-    main_frame = customtkinter.CTkFrame(master=root)
-    main_frame.pack(pady=3, padx=3, fill="both", expand=True)
+        mouse_frame = customtkinter.CTkFrame(master=main_frame)
+        mouse_frame.place(relx=0, rely=0.12, relwidth=0.5, relheight=0.4)
 
-    customtkinter.CTkLabel(master=main_frame, text="Autoclicker", font=font_large).pack()
+        mouse_frame.columnconfigure((0, 1, 2), weight=1, uniform='a')
+        mouse_frame.rowconfigure((0, 1, 2, 3, 4), weight=1, uniform='a')
 
+        customtkinter.CTkLabel(master=mouse_frame, text="Mouse", font=self.font_medium).grid(column=1, row=0, sticky="nswe")
+        customtkinter.CTkLabel(master=mouse_frame, text="Speed", font=self.font_small).grid(column=0, row=1, sticky="w", columnspan=2)
 
+        self.mouse_interval_entry = customtkinter.CTkEntry(master=mouse_frame, font=self.font_mini, placeholder_text="Interval")
+        self.mouse_interval_entry.grid(column=0, row=2, padx=5)
 
+        mouse_cps_entry = customtkinter.CTkEntry(master=mouse_frame, font=self.font_mini, placeholder_text="Cps")
+        mouse_cps_entry.grid(column=2, row=2, padx=5)
 
-#mouse autoclicker frame
-    mouse_frame = customtkinter.CTkFrame(master=main_frame)
-    mouse_frame.place(relx=0, rely=0.12, relwidth=0.5, relheight=0.4)
+        customtkinter.CTkLabel(master=mouse_frame, text="sec", font=self.font_small).grid(column=1, row=2, sticky='w')
 
-    mouse_frame.columnconfigure((0, 1, 2), weight=1, uniform='a')
-    mouse_frame.rowconfigure((0, 1, 2, 3, 4), weight=1, uniform='a')
+        key_frame = customtkinter.CTkFrame(master=main_frame)
+        key_frame.place(relx=0.5, rely=0.12, relwidth=0.5, relheight=0.4)
 
-    customtkinter.CTkLabel(master=mouse_frame, text="Mouse", font=font_medium).grid(column=1, row=0, sticky="nswe")
-    customtkinter.CTkLabel(master=mouse_frame, text="Speed", font=font_small).grid(column=0, row=1, sticky="w", columnspan=2)
+        customtkinter.CTkLabel(master=key_frame, text="Keyboard", font=self.font_medium).pack()
 
-    mouse_intervall_entry = customtkinter.CTkEntry(master=mouse_frame, font=font_mini, placeholder_text="Intervall")
-    mouse_intervall_entry.grid(column=0, row=2, padx=5)
+        operating_frame = customtkinter.CTkFrame(master=main_frame)
+        operating_frame.place(relx=0, rely=0.53, relwidth=1, relheight=0.2)
 
-    mouse_cps_entry = customtkinter.CTkEntry(master=mouse_frame, font=font_mini, placeholder_text="Cps")
-    mouse_cps_entry.grid(column=2, row=2, padx=5)
+        operating_frame.columnconfigure((0, 1, 2), weight=1, uniform='a')
+        operating_frame.rowconfigure((0, 1), weight=1, uniform='a')
 
-    customtkinter.CTkLabel(master=mouse_frame, text="sec", font=font_small).grid(column=1, row=2, sticky='w')
+        start_button = customtkinter.CTkButton(master=operating_frame, text="Start", font=self.font_medium, command=self.start_clicker)
+        start_button.pack(padx=5, side="left")
 
+        stop_button = customtkinter.CTkButton(master=operating_frame, text="Stop", font=self.font_medium, command=self.stop_clicker)
+        stop_button.pack(padx=5, side="right")
 
+    def start_clicker(self):
+        interval = float(self.mouse_interval_entry.get())
 
-
-    #key autoclicker frame
-    key_frame = customtkinter.CTkFrame(master=main_frame)
-    key_frame.place(relx=0.5, rely=0.12, relwidth=0.5, relheight=0.4)
-
-    customtkinter.CTkLabel(master=key_frame, text="Keyboard", font=font_medium).pack()
-
-
-#frame for start/stop etc.
-    operating_frame = customtkinter.CTkFrame(master=main_frame)
-    operating_frame.place(relx=0, rely=0.53, relwidth=1, relheight=0.2)
-
-    operating_frame.columnconfigure((0, 1, 2), weight=1, uniform='a')
-    operating_frame.rowconfigure((0, 1), weight=1, uniform='a')
-
-    start_button = customtkinter.CTkButton(master=operating_frame, text="start", font=font_medium)
-    start_button.pack(padx=5, side="left")
-
-    stop_button = customtkinter.CTkButton(master=operating_frame, text="stop", font=font_medium)
-    stop_button.pack(padx=5, side="right")
+    def stop_clicker(self):
+        pass
 
 
-    root.mainloop()
+if __name__ == "__main__":
+    clicker = Clicker(0.25, Controller())
 
-
-startup()
+    app = AutoClickerApp()
+    app.mainloop()
+    #threading.Thread(target=AutoClickerApp().mainloop).start()
