37b2508b3978f63e395d072b47f88f001556e972
1sythe <sythedev@proton.me>
2024-03-26 03:42:09 +0100

    added ui testing

--- clicker.py
+++ clicker.py
@@ -3,6 +3,10 @@ import threading
 from pynput.mouse import Button, Controller
 
 
+def uitestlog(msg):
+    print(f"[UI TEST MODE] {msg}")
+
+
 class Clicker(threading.Thread):
     def __init__(self, interval, mouse):
         print("[DEBUG] AC Initialized")
@@ -12,26 +16,33 @@ class Clicker(threading.Thread):
         self.button = Button.left
         self.running = False
 
-    def run(self):
-        print("[DEBUG] AC Started")
+    def click(self):
         self.running = True
+        uitestlog("AC would start now")
+        uitestlog(f"[Stats] Interval: {str(self.interval)}s ({1 / self.interval} CPS) | {str(self.button)}")
+        #while self.running:
+        #    self.mouse.click(self.button)
+        #    time.sleep(self.interval)
+
+        # This is only for UI testing purposes
         while self.running:
-            self.mouse.click(self.button)
+            uitestlog("Click")
             time.sleep(self.interval)
 
-        print("[DEBUG] AC Stopped")
+        uitestlog("AC would stop now")
 
     def stop(self):
         self.running = False
 
 
 if __name__ == "__main__":
-    print("You now have 3 seconds to move your mouse to the desired location")
-    time.sleep(3)
-    mouse = Controller()
-    clicker = Clicker(0.25, mouse)
+    clicker = Clicker(0.1, Controller())
     clicker.start()
+
+    print("Clicking in 5 sec")
+    time.sleep(5)
+
+    clicker.click()
     time.sleep(5)
     clicker.stop()
-    clicker.join()
-    print("[DEBUG] AC Finished")
+
